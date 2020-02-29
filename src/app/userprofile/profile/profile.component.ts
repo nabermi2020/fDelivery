53ad1c5058d6002b78e26a74a30c175d73ce722f
M.Naberezhnyi <naber201725@gmail.com>
2020-02-29 18:25:17 +0200

    Added some changes to the 'profile' component

--- src/app/userprofile/profile/profile.component.ts
+++ src/app/userprofile/profile/profile.component.ts
@@ -11,17 +11,37 @@ import { AuthService } from 'src/app/auth/services/auth.service';
 export class ProfileComponent implements OnInit {
   public id: number;
   public user: User;
+  objectKeys = Object.keys;
+  public userViewTemplate = {
+    'First Name': '',
+    'Last Name': '',
+    'Login': '',
+    'Phone': '',
+    'Email': '',
+    'Address': ''
+  };
 
   constructor(private route: ActivatedRoute,
               private authService: AuthService) { }
-
+  
   ngOnInit(): void {
     this.fetchUserInfo();
+    this.mapUserData();
+    console.log(this.userViewTemplate);
   }
 
   private fetchUserInfo(): void {
     this.id = this.route.snapshot.children[0].params.id;
     this.user = this.authService.getUserById(this.id);
+  }
+
+  private mapUserData(): void {
+    this.userViewTemplate['First Name'] = this.user.user.firstName;
+    this.userViewTemplate['Last Name'] = this.user.user.lastName;
+    this.userViewTemplate['Login'] = this.user.user.login;
+    this.userViewTemplate['Phone'] = this.user.user.phone;
+    this.userViewTemplate['Email'] = this.user.user.email;
+    this.userViewTemplate['Address'] = this.user.user.address;
   }
 
 }
